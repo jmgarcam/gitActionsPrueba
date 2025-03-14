69a47ecb8671bace3f842d496397928715e36e6c
jmgarcam <152276518+jmgarcam@users.noreply.github.com>
2025-03-14 10:00:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+import os
 
 # Obtener la hora actual
 hora_actual = datetime.now()
 
 print("Push efectuado a las: " + str(hora_actual))
+
+usuario = os.getenv("GITHUB_ACTOR")
+print(f"El usuario que ejecutó esta acción es: {usuario}")
